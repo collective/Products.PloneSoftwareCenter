8c4078da86bfa709ea1b07cca7219eeb39f9cb73
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-02-16 07:20:36 +0000

    Prepare to move PSC to an egg.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.5'
+
+setup(name='Products.PloneSoftwareCenter',
+      version=version,
+      description="Plone Software Center is a tool to keep track of software projects and software releases, and is used to power the Products download area on plone.org. It was formerly called ArchPackage.",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/collective/Products.PloneSoftwareCenter',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
