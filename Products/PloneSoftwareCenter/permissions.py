f3b7889b1319968f0cfe4272e145a7939fbcd569
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-02-17 01:52:52 +0000

    Move contents of trunk to egg.

---
+++ Products/PloneSoftwareCenter/permissions.py
@@ -0,0 +1,19 @@
+"""
+$Id$
+"""
+from Products.CMFCore.permissions import setDefaultRoles
+
+AddSoftwareCenter = "PloneSoftwareCenter: Add Software Center"
+AddProject = 'PloneSoftwareCenter: Add Project'
+AddReviewComment = 'PloneSoftware: Add Review Comment'
+ApproveProject = 'PloneSoftwareCenter: Approve Project'
+
+# Let members add new projects, but only manager add help centres
+setDefaultRoles(AddSoftwareCenter, ('Manager',))
+
+# Setting this by default and controlling with area workflow means factory
+# works
+setDefaultRoles(AddProject, ('Member','Manager','Owner',))
+
+setDefaultRoles(AddReviewComment, ('Manager', 'PSCEvaluator',))
+setDefaultRoles(ApproveProject, ('Manager', 'PSCEvaluator',))
