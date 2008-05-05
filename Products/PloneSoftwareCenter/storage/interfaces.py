dc6e35c5d250c877adb73ba2ac46ebd4eac5745f
tarek <tarek@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-05 18:48:15 +0000

    completed interface

--- Products/PloneSoftwareCenter/storage/interfaces.py
+++ Products/PloneSoftwareCenter/storage/interfaces.py
@@ -3,3 +3,15 @@ from zope.interface import Interface
 class IPSCFileStorage(Interface):
     """adapter for storing file"""
 
+    def get(name, instance, **kwargs):
+        """returns the file `name` in an archetype instance"""
+
+    def set(name, instance, value, **kwargs):
+        """sets the file `name` in an archetype instance"""
+
+    def unset(name, instance, **kwargs):
+        """removes the file `name` in an archetype instance"""
+    
+    def getName(instance):
+        """returns the storage name"""
+
