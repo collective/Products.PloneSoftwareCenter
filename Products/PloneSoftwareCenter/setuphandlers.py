ad2acae4aa18e118b4b5bea884fdd2d85dd7d095
tarek <tarek@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-09-17 13:59:51 +0000

    fixed migration with real life data (plone.org)

--- Products/PloneSoftwareCenter/setuphandlers.py
+++ Products/PloneSoftwareCenter/setuphandlers.py
@@ -11,6 +11,8 @@ import zipfile
 import re
 import sys
 import subprocess
+import urllib2
+import socket
 
 from StringIO import StringIO
 
@@ -30,7 +32,39 @@ def temp(function):
             shutil.rmtree(working_dir, ignore_errors=True)
     return _temp
 
+def timeout(function):
+    def _timeout(*args, **kw):
+        old = socket.getdefaulttimeout()
+        socket.setdefaulttimeout(4)
+        try:
+            return function(*args, **kw)
+        finally:
+            socket.setdefaulttimeout(old)	
+    return _timeout
 
+class DistantFile(object):
+    def __init__(self, url):
+        try:
+	   self.handle = urllib2.urlopen(url)
+        except (urllib2.HTTPError, urllib2.URLError):
+           self.handle = None
+	self.url = url 
+	self.on_pypi = url.startswith('http://pypi.python.org')
+
+    def getDownloadableFile(self):
+        return self
+
+    def getId(self):
+        url = self.url.split('#md5=')
+	url = url[0]
+        return url.split()[-1] 
+
+    def get_data(self):
+        if self.handle is None:
+            return ""
+        return self.handle.read()   
+ 
+@timeout
 def before_1_5(portal_setup):
     """runs a migration on a previous version of PSC
     
@@ -44,11 +78,17 @@ def before_1_5(portal_setup):
         # for each file in the project we 
         # extract the distutils name
         project_path = '/'.join(project.getPhysicalPath())
-        files = cat(**{'portal_type': 'PSCFile',
+        files = cat(**{'portal_type': ['PSCFile', 'PSCFileLink'],
                        'path': project_path})
         ids = []
         for file_ in files:
-            file_ = file_.getObject()
+            if file_.portal_type == 'PSCFileLink':
+	        # the file is somewhere else, let's scan it
+		file_ = file_.getObject()
+                file_ = DistantFile(file_.getExternalURL())	
+            else:	    
+                file_ = file_.getObject()
+		
             id_ = extract_distutils_id(file_) 
             if id_ is not None and id_ not in ids:
                 ids.append(id_)
@@ -69,18 +109,23 @@ def before_1_5(portal_setup):
         # synchronize distutils ids
         distutils_ids = {}
         for project in psc.objectValues():
+            if project.getId() in ('plone',):
+                logging.info('Skipping %s' % project.getId())
+		continue
             logging.info('Working on %s' % project.getId())
 
             # trying to find distutils ids
             ids = _discovering_dist_ids(project)
         
             for id_ in ids:
+	        logging.info('Found id %s' % id_)
                 old = distutils_ids.get(id_, ())
                 if project in old:
                     continue
                 distutils_ids[id_] = old + (project,) 
-        
+       	 
         # synchronize with the Cheeseshop
+	logging.info('Starting synchro')
         pypi_synchro(distutils_ids)
 
 @temp
@@ -88,6 +133,7 @@ def extract_distutils_id(egg_or_tarball):
     """gives the disutils id"""
     file_ = egg_or_tarball.getDownloadableFile()
     filename = egg_or_tarball.getId()
+    logging.info('Extracting %s' % filename)
     data = file_.get_data()
     if data == '':
         return None
@@ -95,9 +141,13 @@ def extract_distutils_id(egg_or_tarball):
     fileobj = StringIO(data)
     # is it a tarfile (let's trust the extension)
     if filename.split('.')[-2:] == ['tar', 'gz']:
+        logging.info('We have a tarball')
         # Python 2.4's tarfile should be too buggy
         # to extract setup.py
-        tar = tarfile.TarFile.open(filename, fileobj=fileobj, mode='r:gz')
+        try:
+            tar = tarfile.TarFile.open(filename, fileobj=fileobj, mode='r:gz')
+        except tarfile.ReadError:
+            return None	
         first_member = tar.getnames()[0]
         folder = os.path.split(first_member)[0]
         for tarinfo in tar:
@@ -105,7 +155,11 @@ def extract_distutils_id(egg_or_tarball):
         tar.close()
         # let's get into the extracted package
         old = os.getcwd()
+	folder = os.path.join(old, folder)
         os.chdir(folder)
+	# if the file does not have a setup.py, let's quit
+	if 'setup.py' not in os.listdir(folder):
+            return None
         try:
             name = subprocess.Popen([sys.executable, 'setup.py', '--name'], 
                                     stdout=subprocess.PIPE).communicate()[0]
@@ -114,6 +168,7 @@ def extract_distutils_id(egg_or_tarball):
         return name.strip() 
     # its an egg (a zip)
     elif os.path.splitext(filename)[-1] == '.egg':
+        logging.info('We have a zipped egg')
         zip = zipfile.ZipFile(fileobj, 'r')
         try:
             for info in zip.infolist():
@@ -130,8 +185,10 @@ def extract_distutils_id(egg_or_tarball):
 
 def _attribute_distid(project, distid):
     if not project.getDistutilsMainId():
+        logging.info('%s owns %s (main id)' % (project.getId(), distid))     
         project.setDistutilsMainId(distid)
     else:
+        logging.info('%s owns %s (secondary id)' % (project.getId(), distid))
         project.setDistutilsSecondaryIds(distid)
     project.reindexObject()
     logging.info('%s owns %s' % (project.getId(), distid))
@@ -168,11 +225,9 @@ def pypi_synchro(distutils_ids):
             # unavoidable: PSC doesn't act as a pypi mirror
             p = projects[0]
             _attribute_distid(p, distid)
-            logging.info('%s owns %s' % (p.getId(), distid))
             for p2 in projects[1:]:
                 logging.warning('%s conflicts with %s' % \
                                   (p2.getId(), p.getId())) 
-        
         # (author_email, id) is unique at PyPI
         for i, project in enumerate(projects):
             author_email = project.getContactAddress()
