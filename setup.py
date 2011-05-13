45082f4dd1f8422219b5ad5c72b3a1a9c1725b0c
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-13 02:08:24 +0000

    Prepare Products.PloneSoftwareCenter 1.6.2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.6.1'
+version = '1.6.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read() + '\n\n'
