fce45ccbeb03549d004fef9bfcadeb53755e4d1e
Andreas Mantke <maand@gmx.de>
2011-08-24 23:49:45 +0200

    display of more than one license added to the psc_project_view.pt and the necessary changes in release.py

--- Products/PloneSoftwareCenter/content/release.py
+++ Products/PloneSoftwareCenter/content/release.py
@@ -196,7 +196,7 @@ PSCReleaseSchema = OrderedBaseFolderSchema.copy() + Schema((
         vocabulary='getLicenseVocab',
         widget=SelectionWidget(
             format = 'radio',
-            label=_(u"label_release_license", default=u"License"),
+            label=_(u"label_release_license", default=u"Second License (if published under different licenses)"),
             description=_(u"help_release_license", default=u"Release License: Please examine carefully which license you choose for your contribution. You can't  change it after the release."),
             i18n_domain='plonesoftwarecenter',
         ),
@@ -209,7 +209,7 @@ PSCReleaseSchema = OrderedBaseFolderSchema.copy() + Schema((
         vocabulary='getLicenseVocab',
         widget=SelectionWidget(
             format = 'radio',
-            label=_(u"label_release_license", default=u"License"),
+            label=_(u"label_release_license", default=u"Third License (if published under different licenses)"),
             description=_(u"help_release_license", default=u"Release License: Please examine carefully which license you choose for your contribution. You can't  change it after the release."),
             i18n_domain='plonesoftwarecenter',
         ),
